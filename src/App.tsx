27f53bd06f350c02e354d3c13ffbc1a090e4851a
Rajiv5055 <69000253+Rajiv5055@users.noreply.github.com>
2022-06-20 12:28:06 +0530

    teller aasignment

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import {useState,useEffect} from 'react';
+import './App.css';
+import LinkToTeller from './LinkToTeller';
+
+function App() {
+  return (
+    <div className="App">
+    <LinkToTeller/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/LinkToTeller.tsx
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import {useEffect} from 'react';
+import './App.css';
+
+type enrollment = {
+  id: string;
+}
+type institution = {
+  name: string;
+}
+type user = {
+  id: string;
+}
+
+type enrollments = {
+  accessToken: string;
+  enrollment: enrollment;
+  institution: institution;
+  signatures: [];
+  user: user;
+}
+declare const window:any;
+
+function LinkToTeller() {
+  //const [account,setAccount] = useState({});
+
+ 
+  useEffect(() => {
+    
+    async function getAccountDetails(accessToken: String){
+      const headers : any ={
+        'Authorization' : `Bearer ${accessToken}`
+      }
+      await axios.get("https://api.teller.io/accounts", { headers })
+            .then((response: any)=>{
+                console.log(response.account);
+            })
+   }
+
+    const script = document.createElement("script");
+    script.src = "https://cdn.teller.io/connect/connect.js";
+    script.async = true;
+    document.head.appendChild(script);
+    var tellerConnect = window.TellerConnect.setup({
+        applicationId: "app_o2qpi4tealt28kqksq000",
+        environment: "sandbox",
+        onInit: function() {
+          console.log("Teller Connect has initialized");
+        },
+        // Part 3. Handle a successful enrollment's accessToken
+        onSuccess: function(enrollment:enrollments) {
+          console.log("User enrolled successfully ->", enrollment.accessToken);
+          getAccountDetails(enrollment.accessToken);
+        },
+        onExit: function() {
+          console.log("User closed Teller Connect");
+        }
+
+    });
+
+    
+    var el = document.getElementById("myButton");
+    if(el !== null)
+    el.addEventListener("click", function() {
+       tellerConnect.open();
+    });
+  }, []);
+
+
+  return (
+    <div className="App">
+    <button id="myButton" >
+      Open Teller
+    </button>
+    </div>
+  );
+}
+
+
+export default LinkToTeller
